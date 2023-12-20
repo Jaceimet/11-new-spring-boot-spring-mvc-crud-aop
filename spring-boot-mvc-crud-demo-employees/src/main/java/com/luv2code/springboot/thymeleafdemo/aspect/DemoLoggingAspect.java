3d6b56b6ceaa146c307e69d07a56782c062dd63a
Jaceimet <84209692+Jaceimet@users.noreply.github.com>
2023-12-19 22:49:07 -0800

    Error found refactored code from:
     "@Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..)")"
    
     to:
    
    ""@Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")"
    
    test succeeded

--- spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
+++ spring-boot-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/DemoLoggingAspect.java
@@ -2,6 +2,7 @@ package com.luv2code.springboot.thymeleafdemo.aspect;
 
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
@@ -15,21 +16,22 @@ public class DemoLoggingAspect {
     private Logger myLogger = Logger.getLogger(getClass().getName());
 
     // setup pointcut declarations
-    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..)")
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
     private void forControllerPackage(){}
 
     // do the same for service package and dao
-    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..)")
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
     private void forServicePackage(){}
 
 
-    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..)")
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
     private void forDaoPackage(){}
 
     @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
     private void forAppFlow(){}
 
     // Add @Before advice
+    @Before ("forAppFlow()")
     public void before(JoinPoint theJoinPoint){
 
         // display method we are calling
